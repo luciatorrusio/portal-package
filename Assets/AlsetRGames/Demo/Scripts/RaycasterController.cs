2b43f4ebb71b6a3e94d17097625c12e67529e7ea
Lucia Torrusio <lucia.torrusio@mercadolibre.com>
2025-02-15 16:17:30 -0300

    refactor

---
+++ Assets/AlsetRGames/Demo/Scripts/RaycasterController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Physics = AlsetRGames.Portal.Support.Physics;
+
+public class RaycasterController : MonoBehaviour
+{
+    
+    private List<RaycastHit> hitInfo = new List<RaycastHit>();
+    void Update()
+    {
+        var raycastClicked = Input.GetKey(KeyCode.R);
+        if (raycastClicked)
+        {
+            if (Physics.PortalRaycast(transform.position, transform.forward, out hitInfo))
+                print($"amount of rays: {hitInfo.Count}, {hitInfo[hitInfo.Count-1].collider.gameObject.name}");
+            else
+                print("didnt hit nything");
+        }
+        
+    }
+}
